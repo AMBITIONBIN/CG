a789ae912bfa259b8e6fbd3002147d5d9b6b290d
Joyqul Chen <joy1357924680@gmail.com>
2014-03-30 14:36:41 +0800

    Enable 'w' for forward and 's' for backward.

--- HW1/main.cpp
+++ HW1/main.cpp
@@ -4,6 +4,11 @@
 #include <GL/glut.h>
 #endif
 #include <glm/glm.hpp>
+#include "mesh.h"
+
+// load object
+mesh* object;
+int WindowSize[2]; // = w & h
 
 // object
 float red = 0.0f, green = 0.0f, blue = 0.0f;
@@ -14,56 +19,94 @@ float b_red = 0.0f, b_green = 0.0f, b_blue = 0.0f;
 
 // camera
 float camera_angle = 0.0f;
-float camera_lx = 0.0f, camera_lz = -1.0f; //vector
-float camera_x = 0.0f, camera_z = 5.0f; //position of camera
+float camera_lx = -200.0f, camera_lz = -150.0f; //vector
+float camera_x = 200.0f, camera_z = 150.0f; //position of camera
 
-
-void Reshape(int w, int h) {
-
-    // Prevent a divide by zero, when window is too short
-    // (you cant make a window of zero width).
-    if (h == 0)
-        h = 1;
-
-    float ratio =  w * 1.0 / h;
-
-    // Use the Projection Matrix
-    glMatrixMode(GL_PROJECTION);
-
-    // Reset Matrix
-    glLoadIdentity();
-
-    // Set the viewport to be the entire window
-    glViewport(0, 0, w, h);
-
-    // Set the correct perspective.
-    gluPerspective(45.0f, ratio, 0.1f, 100.0f);
-
-    // Get Back to the Modelview
-    glMatrixMode(GL_MODELVIEW);
+void Reshape(GLsizei w, GLsizei h) {
+	WindowSize[0] = w;
+	WindowSize[1] = h;
 }
 
+void light() {
+	GLfloat light_specular[] = {1.0, 1.0, 1.0, 1.0};
+	GLfloat light_diffuse[] = {1.0, 1.0, 1.0, 1.0};
+	GLfloat light_ambient[] = {0.0, 0.0, 0.0, 1.0};
+	GLfloat light_position[] = {150.0, 150.0, 150.0, 1.0};
+
+	glShadeModel(GL_SMOOTH);
+
+	// z buffer enable
+	glEnable(GL_DEPTH_TEST);
+
+	// enable lighting
+	glEnable(GL_LIGHTING);
+	// set light property
+	glEnable(GL_LIGHT0);
+	glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+	glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
+	glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
+}
 
 void RenderScene(void) {
-
+    // Clear buffer
     glClearColor(b_red, b_green, b_blue, 0.0f);
+    glClearDepth(1.0f);
+    glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LEQUAL);
 
     // Clear Color and Depth Buffers
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    glColor3f(red, green, blue);
+    //glColor3f(red, green, blue);
+    glViewport(0, 0, WindowSize[0], WindowSize[1]);
 
     // Reset transformations
+    glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
+
+    //float ratio =  WindowSize[0] * 1.0 / WindowSize[1];
+    //gluPerspective(45.0f, ratio, 0.1f, 100.0f);
+	gluPerspective(60.0, (GLfloat)WindowSize[0]/(GLfloat)WindowSize[1], 1.0, 1000.0);
+
+	// viewing and modeling transformation
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
     // Set the camera
-    gluLookAt(camera_x, 1.0f, camera_z,
-            camera_x + camera_lx, 1.0f,  camera_z + camera_lz,
+    gluLookAt(camera_x, 300.0f, camera_z,
+            camera_x + camera_lx, 1.0f, camera_z + camera_lz,
             0.0f, 1.0f,  0.0f);
 
-    glRotatef(angle, 0.0f, 1.0f, 0.0f);
-    glutSolidTeapot(1.0);
+    //glRotatef(angle, 0.0f, 1.0f, 0.0f);
+    //glutSolidTeapot(1.0);
     //angle+=0.5f;
+    
+	light();
 
+	int lastMaterial = -1;
+	for(size_t i=0;i < object->fTotal;++i) {
+		// set material property if this face used different material
+		if(lastMaterial != object->faceList[i].m) {
+			lastMaterial = (int)object->faceList[i].m;
+			glMaterialfv(GL_FRONT, GL_AMBIENT  , object->mList[lastMaterial].Ka);
+			glMaterialfv(GL_FRONT, GL_DIFFUSE  , object->mList[lastMaterial].Kd);
+			glMaterialfv(GL_FRONT, GL_SPECULAR , object->mList[lastMaterial].Ks);
+			glMaterialfv(GL_FRONT, GL_SHININESS, &object->mList[lastMaterial].Ns);
+
+			//you can obtain the texture name by object->mList[lastMaterial].map_Kd
+			//load them once in the main function before mainloop
+			//bind them in display function here
+		}
+
+		glBegin(GL_TRIANGLES);
+		for (size_t j=0;j<3;++j) {
+			//textex corrd. object->tList[object->faceList[i][j].t].ptr
+			glNormal3fv(object->nList[object->faceList[i][j].n].ptr);
+			glVertex3fv(object->vList[object->faceList[i][j].v].ptr);	
+		}
+		glEnd();
+	}
 
     glutSwapBuffers();
 }
@@ -131,6 +174,8 @@ void ProcessSpecialKeys(int key, int x, int y) {
 
 int main(int argc, char **argv) {
 
+    object = new mesh("box.obj");
+
     // init GLUT and create window
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
@@ -146,9 +191,6 @@ int main(int argc, char **argv) {
     // keyboard detect
     glutKeyboardFunc(ProcessNormalKeys);
     glutSpecialFunc(ProcessSpecialKeys);
-
-    // OpenGL init
-    glEnable(GL_DEPTH_TEST);
 
     // enter GLUT event processing cycle
     glutMainLoop();
