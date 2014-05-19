32256d9bea475f20c1b9318ee7c310337ec0f29e
Joyqul Chen <joy1357924680@gmail.com>
2014-05-19 16:27:07 +0800

    Add code given by TA.

--- HW3/do.sh
+++ HW3/do.sh
@@ -1,1 +1,1 @@
-clang++ main.cpp mesh.cpp scene.cpp -lGLEW -lglut -lGL -lGLU -lX11 -lm -lSOIL -o main && ./main
+clang++ main.cpp mesh.cpp scene.cpp shader.cpp -lGLEW -lglut -lGL -lGLU -lX11 -lm -lSOIL -o main && ./main
